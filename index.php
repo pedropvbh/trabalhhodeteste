7ff89b5ce74cba53de8913ec7a40952aba4f0c82
Gleidson Viana <gle.contato@gmail.com>
2017-09-22 16:24:04 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Teste</title>	
+</head>
+<body>
+<h1>
+Teste inicial
+</h1>
+
+</body>
+</html>
